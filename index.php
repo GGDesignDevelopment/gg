8a2ef720723979a93fc43cb1f0aaa667bb204591
Juan Ageitos <andresageitos@gmail.com>
2017-03-01 22:03:30 -0300

    Initial

--- index.php
+++ index.php
@@ -2,21 +2,42 @@
 <html>
 <head>
 
-	<TITLE>gg creative design</TITLE>
+	<tilte>gg creative design</title>
 	<meta http-equiv="Content-Type" content="text/html; ISO-8859-1">
 	<meta name="DC.Language" SCHEME="RFC1766" CONTENT="Spanish">
-	<meta name="author" content="ignacio Badell">
+	<meta name="author" content="Ignacio Badell">
 	<meta name="descripcion" content="Desarrollo de aplicaciones web y nativas.">
-	<meta name="keywords" content="web,pagina,diseño,creativo,android,iphone,celulares,sitios,instagram,twitter,contenido,2.0">
+	<meta name="keywords" content="web,pagina,diseño,creativo,android,iphone,celulares,sitios,instagram,twitter,contenido,gg,ignacio,juan,ageitos,badell">
 	<meta name="Resource-type" content="Index">
 	<meta name="Revisit-after" content="3 days">
 	<meta name="robots" content="ALL">
-	<link rel="stylesheet" href="./styles/main.css" >
+	<link href="https://fonts.googleapis.com/css?family=Anton|Lobster" rel="stylesheet">
+	<style media="screen">
+		body {
+			height: 100vh;
+			width: 100vw;
+			background: url(./assets/img/background.jpg) no-repeat center center local;
+			-webkit-background-size: cover;
+			-moz-background-size: cover;
+			-o-background-size: cover;
+			background-size: cover;
+			display: flex;
+			flex-flow: row nowrap;
+			justify-content: center;
+			align-items: center;
+			align-content: center;
+		}
+		h1 {
+			font-family: 'lobster', cursive;
+			font-size: 3.5em;
 
+		}
+	</style>
 </head>
 
 <body>
 
+<h1>Proximamente</h1>
 
 
 </body>
